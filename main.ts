393b37769f2f7d4eba18675870ab6deb86475f99
ptquang2000 <81786066+ptquang2000@users.noreply.github.com>
2022-06-12 22:46:59 +0700

    update timeout

--- main.ts
+++ main.ts
@@ -8,37 +8,46 @@ function OnRadioReceivedHandler (name: string, value: number) {
     if (radioID != RADIO_ID) {
         return
     }
+    // ACK
     if (parsedName.length == 3 && dataSeqNums[sensorID] != seqNum) {
         dataSeqNums[sensorID] = seqNum
-        if (sensorID == SENSOR1_ID) {
-            sensor1RadioNames.shift()
-            sensor1RadioValues.shift()
-        }
-        if (sensorID == SENSOR2_ID) {
-            sensor2RadioNames.shift()
-            sensor2RadioValues.shift()
+        for (let index = 0; index <= TOTAL_SENSORS - 1; index++) {
+            if (sensorID == index) {
+                radioNameBuffers[index].shift()
+                radioValueBuffers[index].shift()
+            }
+            // continue releasing if there are still frames left in buffers.
+            if (radioNameBuffers[index].length != 0) {
+                releaseBuffer(index)
+            }
         }
     }
+    // FRAME
     if (parsedName.length == 4) {
-        radio.sendValue("" + (1 - seqNum) + ":" + radioID + ":" + sensorID, -1)
+        // flip sequence number
+        // SENSOR_ID
+        radio.sendValue("" + (1 - seqNum) + ":" + RADIO_ID + ":" + parsedName[2], -1)
         serial.writeString("!" + parsedName[2] + ":" + parsedName[3] + ":" + value + "#")
     }
 }
+// release first frame in buffer
+function releaseBuffer (id: number) {
+    radio.sendValue("" + dataSeqNums[id] + ":" + radioNameBuffers[id][0], parseFloat(radioValueBuffers[id][0]))
+    lastReleaseTimes[id] = control.millis()
+}
 function OnSerialReceivedHandler (cmd: string) {
     if (SENSOR1_CMD.indexOf(cmd) != -1) {
-        sensor1RadioNames.push("" + radioID + ":" + (SENSOR1_ID + 1) + ":" + "LED")
+        sensor1RadioNames.push("" + radioID + ":" + SENSOR1_ID + ":" + "LED")
         sensor1RadioValues.push(cmd)
-        if (dataSeqNums[SENSOR1_ID] == -1) {
-            dataSeqNums[SENSOR1_ID] = 0
-            radio.sendValue("0" + ":" + sensor1RadioNames[0], parseFloat(cmd))
-        }
     }
     if (SENSOR2_CMD.indexOf(cmd) != -1) {
-        sensor2RadioNames.push("" + radioID + ":" + (SENSOR2_ID + 1) + ":" + "LED")
+        sensor2RadioNames.push("" + radioID + ":" + SENSOR2_ID + ":" + "LED")
         sensor2RadioValues.push(cmd)
-        if (dataSeqNums[SENSOR2_ID] == -1) {
-            dataSeqNums[SENSOR2_ID] = 0
-            radio.sendValue("0" + ":" + sensor2RadioNames[0], parseFloat(cmd))
+    }
+    // releasing immediately if buffer is empty
+    for (let index = 0; index <= TOTAL_SENSORS - 1; index++) {
+        if (radioNameBuffers[index].length == 1) {
+            releaseBuffer(index)
         }
     }
 }
@@ -53,6 +62,10 @@ let radioID = ""
 let seqNum = 0
 let parsedName: string[] = []
 let RADIO_ID = ""
+let TOTAL_SENSORS = 0
+let radioValueBuffers: string[][] = []
+let radioNameBuffers: string[][] = []
+let lastReleaseTimes: number[] = []
 let dataSeqNums: number[] = []
 let SENSOR2_CMD: string[] = []
 let SENSOR2_ID = 0
@@ -62,10 +75,9 @@ let SENSOR1_CMD: string[] = []
 let SENSOR1_ID = 0
 let sensor1RadioValues: string[] = []
 let sensor1RadioNames: string[] = []
-let radioValueBuffers: number[] = []
 sensor1RadioNames = []
 sensor1RadioValues = []
-SENSOR1_ID = 0
+SENSOR1_ID = 1
 SENSOR1_CMD = [
 "0",
 "1",
@@ -74,23 +86,28 @@ SENSOR1_CMD = [
 ]
 sensor2RadioNames = []
 sensor2RadioValues = []
-SENSOR2_ID = 0
+SENSOR2_ID = 2
 SENSOR2_CMD = [
 "4",
 "5",
 "6",
 "7"
 ]
-dataSeqNums = [-1, -1]
-let lastReleaseTimes = [0, 0]
+dataSeqNums = [0, 0]
+lastReleaseTimes = [0, 0]
+radioNameBuffers = [sensor1RadioNames, sensor2RadioNames]
+radioValueBuffers = [sensor1RadioValues, sensor2RadioValues]
+let sensorCommands = [SENSOR1_CMD, SENSOR2_CMD]
 let TIMEOUT = 500
+TOTAL_SENSORS = 2
 radio.setGroup(244)
 RADIO_ID = "1813678"
-loops.everyInterval(100, function () {
-    if (sensor1RadioNames.length != 0) {
-        radio.sendValue("" + dataSeqNums[SENSOR1_ID] + ":" + sensor1RadioNames[0], parseFloat(sensor1RadioValues[0]))
-    }
-    if (sensor2RadioNames.length != 0) {
-        radio.sendValue("" + dataSeqNums[SENSOR2_ID] + ":" + sensor2RadioNames[0], parseFloat(sensor2RadioValues[0]))
+control.inBackground(function () {
+    while (true) {
+        for (let index = 0; index <= TOTAL_SENSORS - 1; index++) {
+            if (radioNameBuffers[index].length != 0 && lastReleaseTimes[index] - control.millis() >= TIMEOUT) {
+                releaseBuffer(index)
+            }
+        }
     }
 })
