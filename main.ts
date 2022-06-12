47ff9056143c7f0a99cab206cd383b673a9e4276
ptquang2000 <81786066+ptquang2000@users.noreply.github.com>
2022-06-12 21:46:31 +0700

    add second sensor

--- main.ts
+++ main.ts
@@ -1,32 +1,46 @@
+// name ex:
+// 0:1813678:1:TEMP
 function OnRadioReceivedHandler (name: string, value: number) {
     parsedName = name.split(":")
-    sequenceNum = parseFloat(parsedName[0])
+    seqNum = parseFloat(parsedName[0])
     radioID = parsedName[1]
     sensorID = parseFloat(parsedName[2]) - 1
     if (radioID != RADIO_ID) {
         return
     }
-    if (parsedName.length == 3 && dataSeqNum[sensorID] != sequenceNum) {
-        dataSeqNum[sensorID] = sequenceNum
-        radioNameBuffer.shift()
-        radioValueBuffer.shift()
+    if (parsedName.length == 3 && dataSeqNums[sensorID] != seqNum) {
+        dataSeqNums[sensorID] = seqNum
+        if (sensorID == SENSOR1_ID) {
+            sensor1RadioNames.shift()
+            sensor1RadioValues.shift()
+        }
+        if (sensorID == SENSOR2_ID) {
+            sensor2RadioNames.shift()
+            sensor2RadioValues.shift()
+        }
     }
     if (parsedName.length == 4) {
-        dataType = parsedName[3]
-        radio.sendValue("" + (1 - sequenceNum) + ":" + radioID + ":" + sensorID, -1)
-        serial.writeString("!" + sensorID + ":" + dataType + ":" + value + "#")
+        radio.sendValue("" + (1 - seqNum) + ":" + radioID + ":" + sensorID, -1)
+        serial.writeString("!" + parsedName[2] + ":" + parsedName[3] + ":" + value + "#")
     }
 }
 function OnSerialReceivedHandler (cmd: string) {
-    let list: string[] = []
-    msg = "" + radioID
-    if (list.indexOf(cmd) != -1) {
-        msg = "" + msg + (":" + LED_SENSOR_ID + ":" + "LED")
-    } else {
-        return
+    if (SENSOR1_CMD.indexOf(cmd) != -1) {
+        sensor1RadioNames.push("" + radioID + ":" + (SENSOR1_ID + 1) + ":" + "LED")
+        sensor1RadioValues.push(cmd)
+        if (dataSeqNums[SENSOR1_ID] == -1) {
+            dataSeqNums[SENSOR1_ID] = 0
+            radio.sendValue("0" + ":" + sensor1RadioNames[0], parseFloat(cmd))
+        }
     }
-    radioNameBuffer.push(msg)
-    radioValueBuffer.push(cmd)
+    if (SENSOR2_CMD.indexOf(cmd) != -1) {
+        sensor2RadioNames.push("" + radioID + ":" + (SENSOR2_ID + 1) + ":" + "LED")
+        sensor2RadioValues.push(cmd)
+        if (dataSeqNums[SENSOR2_ID] == -1) {
+            dataSeqNums[SENSOR2_ID] = 0
+            radio.sendValue("0" + ":" + sensor2RadioNames[0], parseFloat(cmd))
+        }
+    }
 }
 serial.onDataReceived(serial.delimiters(Delimiters.Hash), function () {
     OnSerialReceivedHandler(serial.readUntil(serial.delimiters(Delimiters.Hash)))
@@ -34,31 +48,49 @@ serial.onDataReceived(serial.delimiters(Delimiters.Hash), function () {
 radio.onReceivedValue(function (name, value) {
     OnRadioReceivedHandler(name, value)
 })
-let msg = ""
-let dataType = ""
 let sensorID = 0
 let radioID = ""
-let sequenceNum = 0
+let seqNum = 0
 let parsedName: string[] = []
-let LED_SENSOR_ID = 0
 let RADIO_ID = ""
-let dataSeqNum: number[] = []
-let radioValueBuffer: string[] = []
-let radioNameBuffer: string[] = []
-radioNameBuffer = []
-radioValueBuffer = []
-dataSeqNum = [0, 0]
-radio.setGroup(244)
-RADIO_ID = "1813678"
-LED_SENSOR_ID = 1
-let LED_CMD = [
+let dataSeqNums: number[] = []
+let SENSOR2_CMD: string[] = []
+let SENSOR2_ID = 0
+let sensor2RadioValues: string[] = []
+let sensor2RadioNames: string[] = []
+let SENSOR1_CMD: string[] = []
+let SENSOR1_ID = 0
+let sensor1RadioValues: string[] = []
+let sensor1RadioNames: string[] = []
+let radioValueBuffers: number[] = []
+sensor1RadioNames = []
+sensor1RadioValues = []
+SENSOR1_ID = 0
+SENSOR1_CMD = [
 "0",
 "1",
 "2",
 "3"
 ]
+sensor2RadioNames = []
+sensor2RadioValues = []
+SENSOR2_ID = 0
+SENSOR2_CMD = [
+"4",
+"5",
+"6",
+"7"
+]
+dataSeqNums = [-1, -1]
+let lastReleaseTimes = [0, 0]
+let TIMEOUT = 500
+radio.setGroup(244)
+RADIO_ID = "1813678"
 loops.everyInterval(100, function () {
-    if (radioNameBuffer.length != 0) {
-        radio.sendValue("" + dataSeqNum[LED_SENSOR_ID - 1] + ":" + radioNameBuffer[0], parseFloat(radioValueBuffer[0]))
+    if (sensor1RadioNames.length != 0) {
+        radio.sendValue("" + dataSeqNums[SENSOR1_ID] + ":" + sensor1RadioNames[0], parseFloat(sensor1RadioValues[0]))
+    }
+    if (sensor2RadioNames.length != 0) {
+        radio.sendValue("" + dataSeqNums[SENSOR2_ID] + ":" + sensor2RadioNames[0], parseFloat(sensor2RadioValues[0]))
     }
 })
